f5aeb471d44bf73bee75609ccf93515f8ff45bb8
Shamanskiy <shamanskiy.alexander@gmail.com>
2023-01-18 18:28:00 +0000

    add rand package

---
+++ rand/rand.go
@@ -0,0 +1,33 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+const SessionTokenBytes = 32
+
+func Bytes(numBytes int) ([]byte, error) {
+	b := make([]byte, numBytes)
+	numRead, err := rand.Read(b)
+	if err != nil {
+		return nil, fmt.Errorf("bytes: %w", err)
+	}
+	if numRead < numBytes {
+		return nil, fmt.Errorf("bytes: didn't read enough random bytes")
+	}
+	return b, nil
+}
+
+func String(numBytes int) (string, error) {
+	b, err := Bytes(numBytes)
+	if err != nil {
+		return "", fmt.Errorf("string: %w", err)
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+func SessionToken() (string, error) {
+	return String(SessionTokenBytes)
+}
